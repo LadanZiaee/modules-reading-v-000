e2f0ac2990bc182dd88e3a01918cabfef34dcfca
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-11 20:43:31 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,7 +1,7 @@
 module Dance
   
   def twirl 
-    "I'm twirling"
+    "I'm twirling!"
   end
   
   def jump
@@ -9,7 +9,7 @@ module Dance
   end
   
   def pirouette
-    "I'm doing a pirouette"
+    "I'm doing a pirouette!"
   end
   
   def take_a_bow
